ab2476070ee7a7b3727f7cedd56b93ff5bbee5ec
Mohan Gupta <mohangupta@live.com>
2025-02-27 10:26:41 +1100

    adding a demo playground

---
+++ demo-playground/helloworld/custom_block.js
@@ -0,0 +1,16 @@
+Blockly.common.defineBlocksWithJsonArray([
+    {
+        "type": "output_alert",
+        "message0": "alert with message",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "TEXT",
+                "check": "String"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_LISTS_HUE}"
+    }
+]);
