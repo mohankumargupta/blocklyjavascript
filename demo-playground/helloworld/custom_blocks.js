38b34c24652150893ed3e0ebbf9fb45fcc283cdc
Raspberry Pi <raspberrypisig@gmail.com>
2025-02-28 20:56:27 +1100

    function blocks

--- demo-playground/helloworld/custom_blocks.js
+++ demo-playground/helloworld/custom_blocks.js
@@ -1,5 +1,19 @@
 Blockly.common.defineBlocksWithJsonArray([
     {
+        "type": "functions_def",
+        "message0": "def %1",
+        "args0": [
+            {
+                "type": "field_input",
+                "name": "TEXT",
+                "check": "String"
+            }],
+        "inputsInline": true,        
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_PROCEDURES_HUE}",
+    },
+    {
         "type": "functions_call",
         "message0": "call %1",
         "args0": [
