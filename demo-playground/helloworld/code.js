f7051ec714e26b40fe92a166420e3768e5c5b9a3
Raspberry Pi <raspberrypisig@gmail.com>
2025-03-01 16:11:01 +1100

    change xml to look like image

--- demo-playground/helloworld/code.js
+++ demo-playground/helloworld/code.js
@@ -484,8 +484,8 @@ Code.init = function() {
 
   const savedBlocks = `<xml xmlns="https://developers.google.com/blockly/xml">
   <block type="elements_element_textcontent" id="Z%)e/M4NRjUaL/:Q@bB1" x="163" y="38">
-    <field name="ELEMENT">button</field>
-    <field name="TEXTCONTENT">Double Click?</field>
+    <field name="TAG">button</field>
+    <field name="TEXT">Double Click?</field>
     <statement name="STATEMENT">
       <block type="elements_on" id="@ki?B]EqyuUbM6@p]f]R">
         <field name="FIELDNAME">ITEM2</field>
@@ -499,7 +499,7 @@ Code.init = function() {
     <next>
       <block type="elements_script" id="D(JJ$dgY=~bOQSVmXD{j">
         <statement name="STATEMENTS">
-          <block type="functions_def" id="h7utkV+K\`n=}w{VkyGEt">
+          <block type="functions_def" id="h7utkV+K`n=}w{VkyGEt">
             <field name="TEXT">joke</field>
             <statement name="STATEMENTS">
               <block type="output_alert" id="?Y3N-TPH81YM~dez38cM">
@@ -512,7 +512,7 @@ Code.init = function() {
                   <block type="output_alert" id="LzQWd4ypUW48xR3/2c/6">
                     <value name="TEXT">
                       <block type="text" id=".)/qv?CCt5vA9n#F2t*D">
-                        <field name="TEXT">Click Click?</field>
+                        <field name="TEXT">Click Click!</field>
                       </block>
                     </value>
                   </block>
