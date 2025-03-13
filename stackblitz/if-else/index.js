fe7df785439d7acbb240944bd4dbe41150aca764
Raspberry Pi <raspberrypisig@gmail.com>
2025-03-13 11:52:04 +1100

    not sure

---
+++ stackblitz/if-else/index.js
@@ -0,0 +1,29 @@
+const num = 200;
+
+if (num > 100) {
+    console.log("The number is greater than but not equal to 100");
+}
+
+else if (num > 40) {
+    console.log("The number is greater than but not equal to 40");
+}
+
+else {
+    console.log("The number is less than or equal to 40");
+}
+
+console.log("end of if block");
+
+/*
+
+in python(diagram to the right is based on python code below):
+
+num = 200
+if num > 100:
+    print("The number is greater than but not equal to 100")
+elif num > 40:
+    print("The number is greater than but not equal to 40")
+else:
+    print("The number is less than or equal to 40")
+print("end of if block")
+*/
