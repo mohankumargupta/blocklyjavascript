d47a56b18f0352ad37d74112fd1236c95b389b91
Raspberry Pi <raspberrypisig@gmail.com>
2025-03-13 18:59:58 +1100

    multiple fetches

--- stackblitz.js
+++ stackblitz.js
@@ -1,14 +1,28 @@
  function openStackBlitz(folderName) {
+
+  let indexJS = '';
+  let indexCSS = '';
+  let indexHTML = '';
   
-  fetch(`https://raw.githubusercontent.com/mohankumargupta/blocklyjavascript/refs/heads/main/stackblitz/${folderName}/index.js`)    
+  fetch(`https://raw.githubusercontent.com/mohankumargupta/blocklyjavascript/refs/heads/main/stackblitz/${folderName}/index.js`)
   .then(response => response.text())
   .then(text => {
-    console.log(text);
-
+    indexjs = text;
+    return fetch(`https://raw.githubusercontent.com/mohankumargupta/blocklyjavascript/refs/heads/main/stackblitz/${folderName}/index.html`);
+  })
+  .then( response => response.text())
+  .then(text => {
+    indexHTML = text;
+    return fetch(`https://raw.githubusercontent.com/mohankumargupta/blocklyjavascript/refs/heads/main/stackblitz/${folderName}/index.css`);
+  })
+  .then( response => response.text())
+  .then(text => {
+    indexCSS = text;
     StackBlitzSDK.openProject({
       files: {
-        'index.js': text,
-        'index.html': '<html><body><script src="./index.js"></script></body></html>'
+        'index.js': indexJS,
+        'index.html': indexHTML,
+        'index.css': indexCSS,
       },
       template: 'javascript',
       description: 'Live',
